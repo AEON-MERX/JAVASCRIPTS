dc45e46f3a3f8ed569ab432118444a9980c3383f
aeonmerx <103615875+aeonmerx@users.noreply.github.com>
2023-10-22 22:52:08 -0500

    Create curiosidades.js

---
+++ curiosidades.js
@@ -0,0 +1,49 @@
+// Curiosidad 1: El primer programa de computadora
+console.log('1. El primer programa de computadora fue escrito por Ada Lovelace en 1843. Era un algoritmo destinado a ser ejecutado en la Máquina Analítica de Charles Babbage, una de las primeras computadoras mecánicas.');
+
+// Curiosidad 2: El origen de "bug"
+console.log('2. El término "bug" (insecto) en programación se popularizó después de que Grace Hopper encontrara una polilla real atascada en un relé en una computadora en 1947. Desde entonces, se usa para referirse a errores en el código.');
+
+// Curiosidad 3: La velocidad de la luz y las computadoras
+console.log('3. Las diferencias de velocidad entre la CPU y la memoria RAM se deben en parte a que la luz viaja aproximadamente 10 cm en el tiempo que toma a la electricidad recorrer 1 cm en un cable de cobre.');
+
+// Curiosidad 4: La cantidad de código en un automóvil
+console.log('4. Un automóvil moderno puede contener más de 100 millones de líneas de código, superando en complejidad a un sistema operativo como Windows o Linux.');
+
+// Curiosidad 5: El navegador web y el Dinosaurio de Chrome
+console.log('5. El juego del "Dinosaurio" en Chrome, que aparece cuando no hay conexión a internet, se puede jugar presionando la barra espaciadora. Esto se debe a que el juego se basa en tecnologías web, incluso sin conexión.');
+
+// Curiosidad 6: La misteriosa paradoja de la IA
+console.log('6. La paradoja de la inteligencia artificial (IA) es que, aunque las máquinas pueden aprender y tomar decisiones, no comprenden lo que hacen. La IA es fuerte en tareas específicas pero carece de verdadera inteligencia.');
+
+// Curiosidad 7: La dirección IP más grande
+console.log('7. La dirección IPv6 más grande es 340,282,366,920,938,463,463,374,607,431,768,211,456, que es prácticamente inimaginable. IPv6 se introdujo debido a la escasez de direcciones IPv4.');
+
+// Curiosidad 8: El código más antiguo conocido
+console.log('8. El código más antiguo conocido es el "Código de Ur-Nammu", una tabla de leyes sumerias que data de alrededor de 2100 a.C. Si bien no es un código de programación, representa una forma temprana de reglas y lógica.');
+
+// Curiosidad 9: La velocidad de los superordenadores
+console.log('9. Los superordenadores más rápidos pueden realizar miles de billones de cálculos por segundo. Si un humano realizara cálculos a la misma velocidad, tomaría más de 31,000 años para igualar un segundo de operación de un superordenador.');
+
+// Curiosidad 10: El proyecto que cambió el mundo
+console.log('10. El proyecto "World Wide Web" de Tim Berners-Lee, lanzado en 1991, cambió la forma en que compartimos información y transformó la sociedad. La web ha conectado a miles de millones de personas en todo el mundo.');
+
+/ Curiosidad 12: El término 'navegador web' proviene de la idea de navegar por la web.
+console.log("12. El término 'navegador web' proviene de la idea de navegar por la web.");
+
+// Curiosidad 13: Los servidores web Apache y Nginx son dos de los servidores web más populares en Internet.
+console.log("13. Los servidores web Apache y Nginx son dos de los servidores web más populares en Internet.");
+
+// Curiosidad 14: La tecnología blockchain detrás de las criptomonedas como Bitcoin permite un registro inmutable y seguro de transacciones.
+console.log("14. La tecnología blockchain detrás de las criptomonedas como Bitcoin permite un registro inmutable y seguro de transacciones.");
+
+// Curiosidad 15: El código QR (Quick Response) fue inventado en Japón en 1994 y se utiliza para almacenar información en un formato de matriz bidimensional.
+console.log("15. El código QR (Quick Response) fue inventado en Japón en 1994 y se utiliza para almacenar información en un formato de matriz bidimensional.");
+
+// Curiosidad 16: El primer emoticono :-) se utilizó en un mensaje de correo electrónico en 1982.
+console.log("16. El primer emoticono :-) se utilizó en un mensaje de correo electrónico en 1982.");
+
+// Curiosidad 17: El sistema operativo Unix, desarrollado en 1969, ha influido en muchos otros sistemas operativos, incluyendo Linux y macOS.
+console.log("17. El sistema operativo Unix, desarrollado en 1969, ha influido en muchos otros sistemas operativos, incluyendo Linux y macOS.");
+// Curiosidad 18: El término 'spam' para correos electrónicos no deseados proviene de un sketch cómico de Monty Python.
+console.log("18. El término 'spam' para correos electrónicos no deseados prov
